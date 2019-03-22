ee2ec639d5af0119cc06cab5825d589f749edf43
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-22 03:30:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken? (board, index)
     if board != nil && index != nil
       if board[index] == "" || board[index] == " " || board[index] == nil
+        return nil
+      end
     end
 end
